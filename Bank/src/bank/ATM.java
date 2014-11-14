e3b449e8fcd87d4a394b1582df6c91791db51eb3
thomazpicelli <thomazpicelli@gmail.com>
2014-11-14 18:29:05 -0200

    começando leitura da ATM

--- Bank/src/bank/ATM.java
+++ Bank/src/bank/ATM.java
@@ -1,7 +1,16 @@
 package bank;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
 class ATM 
   implements BankAccountOperation {
+    
+    private ArrayList<BankAccount> banks;
     private Bank b; // banco vinculado ao caixa eletrônico
     private double fee; // taxa cobrada pela uso do caixa eletrônico
     
@@ -9,6 +18,26 @@ class ATM
         this.b = b;
         this.fee = fee;
     }
+    
+    public ATM(String filename){
+        banks = new ArrayList<BankAccount>();
+        try{
+            BufferedReader entrada = new BufferedReader(new FileReader(filename));  //leitura
+
+            String numero = entrada.readLine(); 
+            int numDeContas = Integer.parseInt(numero);  //conversão de tipo primitivo
+
+            for(int i = 0; i<numDeContas; i++) { 
+                String line = entrada.readLine();
+                String conta[] = line.split("#");
+                banks.add(i,new XXXXX(Integer.parseInt(conta[1]),conta[2]));       
+            }    
+        }
+        catch(IOException e){
+            System.exit(-1);
+        }
+    }
+
     
     static double doGetBalance(BankAccountOperation bao,int accountNumber){
         return bao.getBalance(accountNumber);
