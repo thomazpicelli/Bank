449650a934b4fd95938c6ef9cd2d91e02657516d
thomazpicelli <thomazpicelli@gmail.com>
2014-11-24 22:36:25 -0200

    SavingsRegular atualizado

--- Bank/src/bank/SavingsRegularAccount.java
+++ Bank/src/bank/SavingsRegularAccount.java
@@ -3,27 +3,27 @@ package bank;
 import java.util.*;
 
 public class SavingsRegularAccount extends BankAccount{
+    private double balanceSavings;  //saldo tipo poupança
+    private double balanceRegular;  //saldo tipo regular
     private double specialBalance;  // limite da conta-corrente
-    private double interestRate;   // taxa de juros
+    private double interestRate;    // taxa de juros
    
-    public SavingsRegularAccount(int accountNumber, String password, String owner, double balance, double specialBalance, double interestRate){
+    public SavingsRegularAccount(int accountNumber, String password, String owner, double balance, double balanceSavings, double balanceRegular, double specialBalance, double interestRate){
         super(accountNumber, password,owner,balance); 
 	this.interestRate=interestRate;
         this.specialBalance=specialBalance;
     }
    
-    public double BalanceSavings(){
-        return (this.balance/2) * (1 + this.interestRate);
+    public double getBalanceSavings(){
+        return (balanceSavings/2) * (1 + interestRate);
     }
     
-    public double BalanceRegular(){
-        return (this.balance/2) + (this.specialBalance);
+    public double getBalanceRegular(){
+        return (balanceRegular/2) + (specialBalance);
     }
-    
-    public String getBalance(){
-        double x = BalanceSavings();
-        double y = BalanceRegular();
-        return System.out.println("Saldo remunerado:" + x + ". Saldo não remunerado:" + y + ".");
+        
+    public void depoosit(double value){
+        balanceRegular = balanceRegular + value/2;
+        balanceSavings = balanceSavings + value/2;
     }
-    
 }
